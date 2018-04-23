bd5547bb9a73bff35063ef1559516d4533513131
Luvuyo Sono <luvuyosono@gmail.com>
2018-04-23 15:55:00 +0200

    modified the template to accept a list of data

--- users.js
+++ users.js
@@ -7,11 +7,12 @@ document.addEventListener('DOMContentLoaded', function(){
     // get a reference to tableBody where users is to be displayed
     var userDataElem = document.querySelector(".userData");
 
+    var userData = { users: [
+        {username: "deelowtrayne", firstName: "Luvuyo", lastName: "Sono", email: "luvuyosono@gmail.com" },
+        {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+        {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+        {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+      ]};
     // compile the template
-    userDataElem.innerHTML = userTemplate({
-        username : 'deelowtrayne',
-        firstName : 'Luvuyo',
-        lastName : 'Sono',
-        email : 'luvuyosono@gmail.com'
-    });
+    userDataElem.innerHTML = userTemplate(userData);
 });
